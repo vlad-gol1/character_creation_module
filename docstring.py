d38a605df296a952da33978a3ce061c95ea9699a
Владислав Голубев <vladsl37@ya.ru>
2022-12-16 18:03:47 +0300

    Add docstring.

---
+++ docstring.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+print(message)
+
+
+def CalculateSquareRoot(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Докстринг."""
+    if your_number <= 0:
+        return
+    print('Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {CalculateSquareRoot(your_number)}')
+
+
+print(message)
+calc(25.5)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -80,7 +81,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,6 +92,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
